1606b87c2aa8c017272f8e9ac813ec938baccd0d
leonardo arena <leonardofarena@gmail.com>
2022-11-15 15:50:08 -0300

    nn

--- src/ui/components/navbar/NavBar1.jsx
+++ src/ui/components/navbar/NavBar1.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import './navbar.css'
 
 export const NavBar1 = () => {
+
+ const [show, setShow] = useState(false);
+
+    const showMenu = () => {
+    if (show) {
+        collapseMenu();
+        setShow(false);
+    } else {
+        setShow(true);
+    }
+    }
+    
+    const collapseMenu = () => {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+    }
+
+
+ 
+
   return (
     <>
     <header>
 
 
         
-    <div className="container-fluid bg-dark px-0">
-        <div className="row g-0 d-none d-lg-flex">
-            <div className="col-lg-6 ps-5 text-start">
-                <div className="h-100 d-inline-flex align-items-center text-white">
-                    <span>Follow Us:</span>
-                    <a className="btn btn-link text-light" href=""><i className="fab fa-facebook-f"></i></a>
-                    <a className="btn btn-link text-light" href=""><i className="fab fa-twitter"></i></a>
-                    <a className="btn btn-link text-light" href=""><i className="fab fa-linkedin-in"></i></a>
-                    <a className="btn btn-link text-light" href=""><i className="fab fa-instagram"></i></a>
-                </div>
-            </div>
-            <div className="col-lg-6 text-end">
-                <div className="h-100 topbar-right d-inline-flex align-items-center text-white py-2 px-5">
-                    <span className="fs-5 fw-bold me-2"><i className="fa fa-phone-alt me-2"></i>Call Us:</span>
-                    <span className="fs-5 fw-bold">+012 345 6789</span>
-                </div>
-            </div>
-        </div>
-    </div>
+  
    
    
      <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-5">
@@ -42,19 +44,28 @@ export const NavBar1 = () => {
         <div className="collapse navbar-collapse" id="navbarCollapse">
             <div className="navbar-nav ms-auto p-4 p-lg-0">
 
-               <NavLink   className={ ({ isActive }) => `nav-item nav-link ${isActive ? 'active':''}` }
-                    to="/home"> Home </NavLink>
 
-                    <NavLink  className={ ({ isActive }) => `nav-item nav-link ${isActive ? 'active':''}` }
-                      to="/portfolio"
-                    >  Portfolio </NavLink>
+            <NavLink to="/home" 
+                 className={ ({ isActive }) => `nav-link  dropdownn  ${isActive ? 'active ':''}` } >  
+            <button className="btn   " data-bs-toggle="collapse" data-bs-target="#navbarCollapse"   >
+            Home
+            </button>            
+                </NavLink>
 
+                <NavLink to="/portfolio"
+                 className={ ({ isActive }) => `nav-link dropdownn   ${isActive ? 'active ':''}` } >  
+            <button className="btn   " data-bs-toggle="collapse" data-bs-target="#navbarCollapse"   >
+            Portfolio
+            </button>            
+                </NavLink>
          
                 
 
 
-                <div className="nav-item dropdownn" >
- <a href="#" className="nav-link " data-bs-toggle="dropdown">Pages</a>
+                <div className="nav-item  dropdownn" >
+                <NavLink  className="nav-link" to="/" >   <button className="btn   " data-bs-toggle="collapse" data-bs-target="#navbarCollapse"   >
+            Pages
+            </button> </NavLink >
   <div className="dropdownn-content bg-light m-0">
   <NavLink  className="nav-link-drop" to="/contables" > <a >Contables</a></NavLink >
   <NavLink  className="nav-link-drop" to="/impositivos" > <a >Impositivos</a></NavLink >
@@ -65,8 +76,13 @@ export const NavBar1 = () => {
 </div>
 
 
-               <NavLink className={ ({ isActive }) => `nav-item nav-link ${isActive ? 'active':''}` }
-                     to="/contact" >Contact </NavLink>
+           <NavLink to="/contact"
+                 className={ ({ isActive }) => `nav-link  dropdownn  ${isActive ? 'active ':''}` } >  
+            <button className="btn   " data-bs-toggle="collapse" data-bs-target="#navbarCollapse"   >
+            Contact
+            </button>            
+                </NavLink>
+
             </div>
 
           
