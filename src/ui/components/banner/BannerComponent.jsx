1eeaef08538a0e206cdebc3a142aa2abcc104bd7
leonardo arena <leonardofarena@gmail.com>
2022-11-17 10:07:22 -0300

    nnn

--- src/ui/components/banner/BannerComponent.jsx
+++ src/ui/components/banner/BannerComponent.jsx
@@ -6,8 +6,9 @@ export const BannerComponent = ({title}) => {
     <div className="container">
         <div className="row">
             <div className="col-md-12">
-            <h1 className="display-3 text-white animated slideInRight">{title}</h1>
+          
                 <div className="tagline-message text-center">
+                <h1 className="display-3 text-white animated slideInRight">{title}</h1>
                     <h3>Ofrecemos soluciones integrales instalando pisos y revestimientos vinilicos en todo el País.</h3>
                 </div>
             </div>
