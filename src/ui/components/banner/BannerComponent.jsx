95c9a2303df05d18b5ae3b6ea023ebd9466f09a2
leonardo arena <leonardofarena@gmail.com>
2022-11-17 10:43:57 -0300

    nnn

--- src/ui/components/banner/BannerComponent.jsx
+++ src/ui/components/banner/BannerComponent.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const BannerComponent = ({descrip}) => {
   return (
-    <section classNameName="section db p120">
-    <div className="container  page-header">
+    <section classNameName="section ">
+    <div className="container-fluid page-header   py-5 mb-5 wow fadeIn">
         <div className="row">
-            <div className="col-md-12">
+            <div className="col-md-12  text-white">
           
-                <div className="tagline-message text-center">
+                <div className="text-white ">
              
-                    <h3>{descrip}</h3>
+                    <h3 className="text-white ">{descrip}</h3>
                 </div>
             </div>
         </div>
--- src/ui/components/header/HeaderComponent.jsx
+++ src/ui/components/header/HeaderComponent.jsx
@@ -5,7 +5,7 @@ export const HeaderComponent = ({title}) => {
 <>
 <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div className="container py-5">
-        
+            <h1 className="display-3 text-white animated slideInRight">{title}</h1>
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb text-white  animated slideInRight mb-0">                    
                     <li className="breadcrumb-item"><a href="#">Pages</a></li>
