383ce48e447b40291163b3cd8d47b56fd282d386
leonardo arena <leonardofarena@gmail.com>
2022-11-17 10:15:10 -0300

    nnn

--- src/components/homecomponent/HomeComponent.jsx
+++ src/components/homecomponent/HomeComponent.jsx
@@ -4,7 +4,7 @@ import { BannerComponent, HeaderComponent } from '../../ui'
 export const HomeComponent = () => {
   return (
    <>
-  <HeaderComponent title="Home" />
+
   <BannerComponent  descrip="Ofrecemos soluciones integrales instalando pisos y revestimientos vinilicos en todo el País."  />
 
   
