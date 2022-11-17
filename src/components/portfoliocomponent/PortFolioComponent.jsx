2dfecbff311278c1dd083d75e7e346ef32eeb03e
leonardo arena <leonardofarena@gmail.com>
2022-11-17 11:53:12 -0300

    n

--- src/components/portfoliocomponent/PortFolioComponent.jsx
+++ src/components/portfoliocomponent/PortFolioComponent.jsx
@@ -4,7 +4,7 @@ import {  BannerPortfolio} from '../../ui'
 export const PortFolioComponent = () => {
   return (
     <>
- <br /><br /><br /><br /><br />
+
  <BannerPortfolio descrip="Nuestras Obras"  />
     
     </>
