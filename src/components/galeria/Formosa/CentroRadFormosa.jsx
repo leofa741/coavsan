a19ec4fd27bf723857998b26f6775512277245af
leonardo arena <leonardofarena@gmail.com>
2022-11-18 18:21:54 -0300

    nnn

--- src/components/galeria/Formosa/CentroRadFormosa.jsx
+++ src/components/galeria/Formosa/CentroRadFormosa.jsx
@@ -24,51 +24,103 @@ export const CentroRadFormosa=()=> {
 
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/1.jpeg?raw=true',
     title: 'Bed',
   },
   {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/2.jpeg?raw=true',
     title: 'Books',
   },
   {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/3.jpeg?raw=true',
     title: 'Sink',
   },
   {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/4.jpeg?raw=true',
     title: 'Kitchen',
   },
   {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/5.jpeg?raw=true',
     title: 'Blinds',
   },
   {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/6.jpeg?raw=true',
     title: 'Chairs',
   },
   {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/7.jpeg?raw=true',
     title: 'Laptop',
   },
   {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/8.jpeg?raw=true',
     title: 'Doors',
   },
   {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/9.jpeg?raw=true',
     title: 'Coffee',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/10.jpeg?raw=true',
     title: 'Storage',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/11.jpeg?raw=true',
     title: 'Candle',
   },
   {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    img: 'https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/12.jpeg?raw=true',
     title: 'Coffee table',
   },
+    {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/13.jpeg?raw=true',
+    title: 'Coffee table',
+    },
+    {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/14.jpeg?raw=true',
+    title: 'Coffee table',
+     },
+     {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/15.jpeg?raw=true',
+    title: 'Coffee table',
+     },
+    {
+     img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/16.jpeg?raw=true',
+     title: 'Coffee table',
+  },
+  {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/17.jpeg?raw=true',
+    title: 'Coffee table',
+    },
+    {
+        img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/18.jpeg?raw=true',
+        title: 'Coffee table',
+     },
+ {
+ img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/19.jpeg?raw=true',
+  title: 'Coffee table',
+ },
+ {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/20.jpeg?raw=true',
+    title: 'Coffee table',
+    },
+    {
+        img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/21.jpeg?raw=true',
+        title: 'Coffee table',
+        },
+         {
+ img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/22.jpeg?raw=true',
+     title: 'Coffee table',
+ },
+ {
+    img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/23.jpeg?raw=true',
+    title: 'Coffee table',
+    },
+    {
+        img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/24.jpeg?raw=true',
+        title: 'Coffee table',
+        },
+        {
+            img: ' https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/25.jpeg?raw=true',
+            title: 'Coffee table',
+            },
 ];
