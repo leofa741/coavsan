7829c3a65fc681e0ba1a2d89995ea4fd3674a142
leonardo arena <leonardofarena@gmail.com>
2022-11-18 19:22:57 -0300

    nnn

--- src/components/portfoliocomponent/ContenidoPortfolio.jsx
+++ src/components/portfoliocomponent/ContenidoPortfolio.jsx
@@ -16,9 +16,9 @@ export const ContenidoPortfolio = () => {
 
                 <div className="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
                     <div className="service-item">
-                        <img className="img-fluid" src="./src/assets/img/centroradiologicoformosa/51.jpeg" alt=""/>
+                        <img className="img-fluid" src="https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/11.jpeg?raw=true" alt=""/>
                         <div className="service-img">
-                            <img className="img-fluid" src="./src/assets/img/centroradiologicoformosa/51.jpeg" alt=""/>
+                            <img className="img-fluid" src="https://github.com/leofa741/coavsan/blob/master/src/assets/img/centroradiologicoformosa/51.jpeg?raw=true" alt=""/>
                         </div>
                         <div className="service-detail">
                             <div className="service-title">
