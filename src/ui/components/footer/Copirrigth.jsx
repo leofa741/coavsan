2154e7529b5f6d784b262c2e0e95e0d9f8fb191e
leonardo arena <leonardofarena@gmail.com>
2022-11-18 09:46:45 -0300

    n

--- src/ui/components/footer/Copirrigth.jsx
+++ src/ui/components/footer/Copirrigth.jsx
@@ -6,11 +6,11 @@ export const Copirrigth = () => {
     
     <div className="container-fluid copyright bg-dark py-4">
         <div className="container text-center">
-            <p className="mb-2">Copyright &copy; <a className="fw-semi-bold" href="#">Your Site Name</a>, All Right Reserved.
+            <p className="mb-2">Copyright &copy; <a className="fw-semi-bold" href="#">Coa Revestimientos</a>, All Right Reserved.
             </p>
           
-            <p className="mb-0">Designed By <a className="fw-semi-bold" href="https://htmlcodex.com">HTML Codex</a> Distributed
-                By: <a href="https://themewagon.com">ThemeWagon</a> </p>
+            <p className="mb-0">Designed By <a className="fw-semi-bold" href="https://www.puentesdigitales.com.ar/">PuentesDigitales Codex</a> Distributed
+                By: <a href="https://www.puentesdigitales.com.ar/">PuentesDigitales</a> </p>
         </div>
     </div>
     
--- src/ui/components/footer/Footer.jsx
+++ src/ui/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (
@@ -6,31 +7,29 @@ export const Footer = () => {
     <div className="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
         <div className="container py-5">
             <div className="row g-5">
-                <div className="col-lg-3 col-md-6">
+                <div className="col-lg-4 col-md-6">
                     <h5 className="text-white mb-4">Our Office</h5>
-                    <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>San Vicente, Bs As, Argentina</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>11-3313-8900</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>11-3324-9832</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>11-6235-0978</p>
+                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>cubas_beto@hotmail.com</p>
                     <div className="d-flex pt-3">
-                        <a className="btn btn-square btn-primary rounded-circle me-2" href=""><i
-                                className="fab fa-twitter"></i></a>
-                        <a className="btn btn-square btn-primary rounded-circle me-2" href=""><i
-                                className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-square btn-primary rounded-circle me-2" href=""><i
-                                className="fab fa-youtube"></i></a>
-                        <a className="btn btn-square btn-primary rounded-circle me-2" href=""><i
-                                className="fab fa-linkedin-in"></i></a>
+                   
+                                <Link to="/home" className="btn btn-square btn-primary rounded-circle me-2" href=""> <i className="fab fa-whatsapp"></i></Link>
+                                <Link to="/home" className="btn btn-square btn-primary rounded-circle me-2" href=""> <i className="fab fa-facebook-f"></i></Link>
+                                <Link to="/home" className="btn btn-square btn-primary rounded-circle me-2" href=""> <i className="fab fa-instagram"></i></Link>
+
                     </div>
                 </div>
-                <div className="col-lg-3 col-md-6">
+                <div className="col-lg-4 col-md-6">
                     <h5 className="text-white mb-4">Quick Links</h5>
-                    <a className="btn btn-link" href="">About Us</a>
-                    <a className="btn btn-link" href="">Contact Us</a>
-                    <a className="btn btn-link" href="">Our Services</a>
-                    <a className="btn btn-link" href="">Terms & Condition</a>
-                    <a className="btn btn-link" href="">Support</a>
+                    <Link to="/home" className="text-white btn btn-link">Home</Link>
+                    <Link to="/portfolio" className="text-white btn btn-link">Portfolio</Link>
+                    <Link to="/contact" className="text-white btn btn-link">Contacto</Link>
+              
                 </div>
-                <div className="col-lg-3 col-md-6">
+                <div className="col-lg-4 col-md-6">
                     <h5 className="text-white mb-4">Business Hours</h5>
                     <p className="mb-1">Monday - Friday</p>
                     <h6 className="text-light">09:00 am - 07:00 pm</h6>
@@ -39,7 +38,7 @@ export const Footer = () => {
                     <p className="mb-1">Sunday</p>
                     <h6 className="text-light">Closed</h6>
                 </div>
-                <div className="col-lg-3 col-md-6">
+                {/* <div className="col-lg-3 col-md-6">
                     <h5 className="text-white mb-4">Newsletter</h5>
                     <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
                     <div className="position-relative w-100">
@@ -48,7 +47,7 @@ export const Footer = () => {
                         <button type="button"
                             className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     </div>
