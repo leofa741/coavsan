8d52fe7f89858f3e5027cdb123f35d5f2728fb5b
leonardo arena <leonardofarena@gmail.com>
2022-11-18 10:09:43 -0300

    nn

--- src/ui/components/footer/Copirrigth.jsx
+++ src/ui/components/footer/Copirrigth.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 export const Copirrigth = () => {
+  const url="https://www.puentesdigitales.com.ar/";
+  const year = new Date().getFullYear();
+  const name = "Puentes Digitales";
+  
   return (
     <>
     
     <div className="container-fluid copyright bg-dark py-4">
         <div className="container text-center">
-            <p className="mb-2">Copyright &copy; <a className="fw-semi-bold" href="#">Coa Revestimientos</a>, All Right Reserved.
+            <p className="mb-2">Copyright &copy; {year}<a className="fw-semi-bold" href="#">Coa Revestimientos</a>, All Right Reserved.
             </p>
           
-            <p className="mb-0">Designed By <a className="fw-semi-bold" href="https://www.puentesdigitales.com.ar/">PuentesDigitales Codex</a> Distributed
-                By: <a href="https://www.puentesdigitales.com.ar/">PuentesDigitales</a> </p>
+            <p className="mb-0">Designed By <a className="fw-semi-bold" href={url}>{name} Codex</a> Distributed
+                By: <a href={url}>{name}</a> </p>
         </div>
     </div>
     
