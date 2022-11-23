b1dc4bb914ad6e689a17a3c15f8b2f7b045b6f8d
leonardo arena <leonardofarena@gmail.com>
2022-11-22 21:03:28 -0300

    nnn

--- src/components/portfoliocomponent/ContenidoPortfolio.jsx
+++ src/components/portfoliocomponent/ContenidoPortfolio.jsx
@@ -100,12 +100,15 @@ export const ContenidoPortfolio = () => {
                         <div className="service-detail">
                             <div className="service-title">
                                 <hr className="w-25"/>
-                                <h3 className="mb-0">Plumbing & Water Treatment</h3>
+                                <h3 className="mb-0">
+                                Laboratorio Baradero
+                                </h3>
                                 <hr className="w-25"/>
                             </div>
                             <div className="service-text">
-                                <p className="text-white mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos
-                                    lorem sed diam stet diam sed stet.</p>
+                                <p className="text-white mb-0">
+                                Laboratorio Baradero Buenos Aires Argentina
+                                </p>
                             </div>
                         </div>
                         <Link to="/baradero" className="btn btn-primary">Ver más</Link>
@@ -122,15 +125,18 @@ export const ContenidoPortfolio = () => {
                         <div className="service-detail">
                             <div className="service-title">
                                 <hr className="w-25"/>
-                                <h3 className="mb-0">Civil & Gas Engineering</h3>
+                                <h3 className="mb-0">
+                                Laboratorio Chacarita
+                                </h3>
                                 <hr className="w-25"/>
                             </div>
                             <div className="service-text">
-                                <p className="text-white mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos
-                                    lorem sed diam stet diam sed stet.</p>
+                                <p className="text-white mb-0">
+                                Laboratorio Chacarita Buenos Aires Argentina
+                                </p>
                             </div>
                         </div>
-                        <a className="btn btn-light" href="">Read More</a>
+                        <Link to="/laboratoriochacarita" className="btn btn-primary">Ver más</Link>
                     </div>
                 </div>
                 <div className="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
