a9bee4a79a3442763c69091527c059621d17cfb2
leonardo arena <leonardofarena@gmail.com>
2022-12-08 21:15:47 -0300

    nnn

--- src/pages/Baradero.jsx
+++ src/pages/Baradero.jsx
@@ -22,7 +22,7 @@ export const Baradero = () => {
     descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos y revestimientos en paredes con revestimientos de vinilicos y pintura"}
     titulo1={"Pisos"}
     descrip1={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
-    titulo2={"Paredes"}
+    titulo2={"Caracteristicas"}
     descrip2={"revestimientos en paredes con revestimientos de vinilicos y pintura"}
     titulo3={"Pintura"}
     descrip3={"pintura"}
--- src/pages/RadiologiaFormosa.jsx
+++ src/pages/RadiologiaFormosa.jsx
@@ -18,13 +18,13 @@ export const RadiologiaFormosa = () => {
     children={<GaleriaImgComponent tittle="Formosa" />}
     titulo={"Formosa"}
     subtitulo={"Centro Radiológico Formosa"}
-    descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos y revestimientos en paredes con revestimientos de vinilicos y pintura"}
+    descrip={"instalación de revestimientos en pisos con revestimiento vinilicos "}
     titulo1={"Pisos"}
-    descrip1={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
-    titulo2={"Paredes"}
-    descrip2={"revestimientos en paredes con revestimientos de vinilicos y pintura"}
-    titulo3={"Pintura"}
-    descrip3={"pintura"}
+    descrip1={"instalación de revestimientos en pisos con revestimiento de vinilicos"}
+    titulo2={"Caracteristicas"}
+    descrip2={"Revestimiento de PVC homogéneo, de alto rendimiento, permite la construcción de áreas limpias según normas GMP Clase A"}
+    titulo3={"Descripción"}
+    descrip3={"Rollos de 2 m de ancho y 2 mm de espesor Innovador diseño no direccional y policromo, disponible en 24 colores"}
     />      
    </>
   )
