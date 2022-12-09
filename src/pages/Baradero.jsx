1ca4546c5a2acb9c4fcaf1feb1baa70192b00487
leonardo arena <leonardofarena@gmail.com>
2022-12-08 21:20:40 -0300

    nnnn

--- src/pages/Baradero.jsx
+++ src/pages/Baradero.jsx
@@ -19,13 +19,14 @@ export const Baradero = () => {
     
     titulo={"Clinica Baradero"}
     subtitulo={"Clinica Baradero"}
-    descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos y revestimientos en paredes con revestimientos de vinilicos y pintura"}
+    descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
     titulo1={"Pisos"}
-    descrip1={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
+    descrip1={"instalación de revestimientos en pisos con revestimiento de vinilicos"}
     titulo2={"Caracteristicas"}
-    descrip2={"revestimientos en paredes con revestimientos de vinilicos y pintura"}
-    titulo3={"Pintura"}
-    descrip3={"pintura"}
+    descrip2={"Revestimiento de PVC homogéneo, de alto rendimiento, permite la construcción de áreas limpias según normas GMP Clase A"}
+    titulo3={"Descripción"}
+    descrip3={"Rollos de 2 m de ancho y 2 mm de espesor Innovador diseño no direccional y policromo, disponible en 24 colores"}
+    />   
     />      
    </>
   )
--- src/pages/HospitalLagunaBlanca.jsx
+++ src/pages/HospitalLagunaBlanca.jsx
@@ -18,13 +18,14 @@ export const HospitalLagunaBlanca = () => {
 
     titulo={"Hospital Laguna Blanca"}
     subtitulo={"Hospital Laguna Blanca"}
-    descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos y revestimientos en paredes con revestimientos de vinilicos y pintura"}
+    descrip={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
     titulo1={"Pisos"}
-    descrip1={"instalación de revestimientos en pisos con revestimiento de baldosas vinilicos"}
-    titulo2={"Paredes"}
-    descrip2={"revestimientos en paredes con revestimientos de vinilicos y pintura"}
-    titulo3={"Pintura"}
-    descrip3={"pintura"}
+    descrip1={"instalación de revestimientos en pisos con revestimiento de vinilicos"}
+    titulo2={"Caracteristicas"}
+    descrip2={"Revestimiento de PVC homogéneo, de alto rendimiento, permite la construcción de áreas limpias según normas GMP Clase A"}
+    titulo3={"Descripción"}
+    descrip3={"Rollos de 2 m de ancho y 2 mm de espesor Innovador diseño no direccional y policromo, disponible en 24 colores"}
+    />   
     />      
    </>
   )
