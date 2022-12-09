780998179e90e686ef2f8d18b6132f9725c329ab
leonardo arena <leonardofarena@gmail.com>
2022-12-08 21:51:56 -0300

    j

--- src/ui/components/carrousel/CarrouselComponent.jsx
+++ src/ui/components/carrousel/CarrouselComponent.jsx
@@ -15,7 +15,7 @@ export const CarrouselComponent = () => {
                             <div className="row justify-content-center">
                                 <div className="col-lg-10 text-start">
                                     <p className="fs-5 fw-medium text-primary text-uppercase animated slideInRight">25 años de experiencia instalando</p>
-                                    <h1 className="display-1 text-white mb-5 animated slideInRight">Pisos vinilicos</h1>
+                                    <h1 className="display-1 text-white mb-5 animated slideInRight">Pisos vinilicos y Pvc</h1>
                                  
                                 </div>
                             </div>
