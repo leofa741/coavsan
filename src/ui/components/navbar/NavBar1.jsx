319d960c9aa6459a59a1a9cceb61acf77e34548f
leonardo arena <leonardofarena@gmail.com>
2022-12-13 15:14:42 -0300

    n

--- src/ui/components/navbar/NavBar1.jsx
+++ src/ui/components/navbar/NavBar1.jsx
@@ -94,7 +94,7 @@ export const NavBar1 = () => {
                 fontSize: "14px",
                 fontWeight: "bold",
                 textAlign: "left",
-                textShadow: "2px 3px 3px #776A6A",
+                textShadow: "3px 3px 2px #000000",
                 textTransform: "uppercase",
                 textDecorationColor: "white",
                 textDecorationStyle: "double",
