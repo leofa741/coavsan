c8767ae0688ccbf8f4a916e5183bf4bc15632b91
leonardo arena <leonardofarena@gmail.com>
2022-12-15 10:59:09 -0300

    jj

--- src/pages/CoaVideos.jsx
+++ src/pages/CoaVideos.jsx
@@ -6,7 +6,7 @@ export const CoaVideos = () => {
     <>
     <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
       <div className="container py-5">
-          <h1 className="display-3 text-white animated slideInRight">404 Error</h1>
+          <h1 className="display-3 text-white animated slideInRight">Coa en Acción</h1>
           <nav aria-label="breadcrumb">
               <ol className="breadcrumb animated slideInRight mb-0">
                   <li className="breadcrumb-item"><a href="#">Home</a></li>
