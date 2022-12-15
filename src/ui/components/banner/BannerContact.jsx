00eafdc608338236da729bed0668c3fa81ce4626
leonardo arena <leonardofarena@gmail.com>
2022-12-15 11:06:39 -0300

    n

--- src/ui/components/banner/BannerContact.jsx
+++ src/ui/components/banner/BannerContact.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 export const BannerContact = ({descrip}) => { 
   return (
     <section className="section ">
+        <br/>
+        <br/>
+        <br/>
+        <br/>
     <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
       <div className="container py-5">
           <h1 className="display-3 text-white animated slideInRight">Coa en Acción</h1>
