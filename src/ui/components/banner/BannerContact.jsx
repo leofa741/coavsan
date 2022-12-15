cbc20f7e560ec3e5bbc8dc0f6738b9d64627924b
leonardo arena <leonardofarena@gmail.com>
2022-12-15 11:10:58 -0300

    n

--- src/ui/components/banner/BannerContact.jsx
+++ src/ui/components/banner/BannerContact.jsx
@@ -13,7 +13,7 @@ export const BannerContact = ({descrip}) => {
             style={{
                 color: "white !important",
                 fontFamily: "Roboto",
-                fontSize: "90px",
+                fontSize: "60px",
                 fontWeight: "bold",
                 textAlign: "left",
                 textShadow: "2px 2px 4px #000000",
