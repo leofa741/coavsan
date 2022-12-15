5a3bdadb5cf275db8b3bc2e01cdb440ad29d20bb
leonardo arena <leonardofarena@gmail.com>
2022-12-15 11:00:05 -0300

    pp

--- src/pages/CoaVideos.jsx
+++ src/pages/CoaVideos.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom'
 export const CoaVideos = () => {
   return (
     <>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
     <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
       <div className="container py-5">
           <h1 className="display-3 text-white animated slideInRight">Coa en Acción</h1>
