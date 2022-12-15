40f0db19213168ea5d4197b2e2d7f66d52190287
leonardo arena <leonardofarena@gmail.com>
2022-12-15 11:45:24 -0300

    n

--- src/ui/components/banner/BannerPortfolio.jsx
+++ src/ui/components/banner/BannerPortfolio.jsx
@@ -5,8 +5,7 @@ export const BannerPortfolio = ({descrip,titulo}) => {
     <section className="section ">
     <br/>
     <br/>
-    <br/>
-    <br/>
+   
 <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
   <div className="container py-5">
       <h1 className="display-3 text-white animated slideInRight"
