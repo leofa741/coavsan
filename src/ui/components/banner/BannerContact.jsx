251367463847a8a30b7376f95bd53d4bbd3576aa
leonardo arena <leonardofarena@gmail.com>
2022-12-15 11:05:28 -0300

    oo

--- src/ui/components/banner/BannerContact.jsx
+++ src/ui/components/banner/BannerContact.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-export const BannerContact = ({descrip}) => {
+export const BannerContact = ({descrip}) => { 
   return (
     <section className="section ">
-    <div className="container-fluid page-header-contact   py-5 mb-5 wow fadeIn">
-        <div className="row">
-            <div className="col-md-12  text-white">
-          
-                <div className="text-white ">
-             
-                    <h3 className="text-white ">{descrip}</h3>
-                </div>
-            </div>
-        </div>
-    </div>
+    <div className="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+      <div className="container py-5">
+          <h1 className="display-3 text-white animated slideInRight">Coa en Acción</h1>
+          <nav aria-label="breadcrumb">
+              <ol className="breadcrumb animated slideInRight mb-0">
+                  <li className="breadcrumb-item"><a href="#">Home</a></li>
+                  <li className="breadcrumb-item"><a href="#">Pages</a></li>
+                  <li className="breadcrumb-item active" aria-current="page">{descrip}</li>
+              </ol>
+          </nav>
+      </div>
+  </div>
 </section>
   )
 }
